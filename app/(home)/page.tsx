f56b4b8ead8447fba23deaa4bebbdc2ae1f371a9
minhanPark <alsgks1218@naver.com>
2024-02-13 14:47:43 +0900

    loading으로 스트리밍 확인, promiseAll로 병렬 요청도 해보기

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,29 @@
+import Link from "next/link";
+
+export const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
 export const metadata = {
   title: "Home",
 };
 
-export default function Page() {
-  return <h1>Hello Nextjs</h1>;
+async function getMovies() {
+  // 페이지가 켜지기까지 5초의 로딩시간을 준다
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  // 로딩 페이지를 만들지 않아서 느려지는 것을 눈으로 확인 가능했음.
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function Page() {
+  const movies = await getMovies();
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,9 +1,35 @@
+import { URL } from "../../../(home)/page";
+
 type Props = {
   params: {
     id: string;
   };
 };
 
-export default function Page({ params: { id } }: Props) {
-  return <h1>Movie {id}</h1>;
+async function getMovie(id: string) {
+  console.log(`Fetching movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(`${URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(`${URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function Page({ params: { id } }: Props) {
+  // 지금 상태에서는 waterfall이 일어나고 있어서 두번째 함수가 첫번째 함수가 끝나야 실행됨
+  // console.log("start fetching");
+  // const movie = await getMovie(id);
+  // const videos = await getVideos(id);
+  // console.log("end fetching");
+
+  // 지금 상태에서는 병렬적으로 데이터를 갖고오고 있어서 위에보다 좋음
+  console.log("start fetching");
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  console.log("end fetching");
+  return <h1>Movie is {movie.title}</h1>;
 }
