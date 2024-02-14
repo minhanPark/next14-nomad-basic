7a29da1568995d5b7f9a37bc161fb39e2dcec8a9
minhanPark <alsgks1218@naver.com>
2024-02-14 13:19:15 +0900

    suspense로 streaming 하는 부분을 분리해서 데이터를 나눠받을 수 있음

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,24 +1,13 @@
+import { Suspense } from "react";
 import { URL } from "../../../(home)/page";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
 type Props = {
   params: {
     id: string;
   };
 };
-
-async function getMovie(id: string) {
-  console.log(`Fetching movies: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch(`${URL}/${id}`);
-  return response.json();
-}
-
-async function getVideos(id: string) {
-  console.log(`Fetching videos: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch(`${URL}/${id}/videos`);
-  return response.json();
-}
 
 export default async function Page({ params: { id } }: Props) {
   // 지금 상태에서는 waterfall이 일어나고 있어서 두번째 함수가 첫번째 함수가 끝나야 실행됨
@@ -28,8 +17,15 @@ export default async function Page({ params: { id } }: Props) {
   // console.log("end fetching");
 
   // 지금 상태에서는 병렬적으로 데이터를 갖고오고 있어서 위에보다 좋음
-  console.log("start fetching");
-  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-  console.log("end fetching");
-  return <h1>Movie is {movie.title}</h1>;
+
+  return (
+    <div className="">
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
